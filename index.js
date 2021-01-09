5d8fd325ec557249eb0b01a23d451a91def86ec7
nakamura196 <na.kamura.1263@gmail.com>
2021-01-10 07:00:42 +0900

    add data

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(3000);
+const express = require('express'); 
+const app = express();
 
-console.log('Server running at http://127.0.0.1:3000');
+// axios を require してインスタンスを生成する
+const axiosBase = require('axios');
+const axios = axiosBase.create({
+  //baseURL: 'http://localhost:4000', // バックエンドB のURL:port を指定する
+  headers: {
+    'Content-Type': 'application/json',
+    //'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'  
+});
+
+
+// GET API定義
+app.get('/', (req, res) => {
+
+ //end('GETパラメータ取得: ' + );
+
+ const uri = req.query.u
+
+ // [2] バックエンドB に対してリクエストを投げる
+ axios.get(uri)
+ .then(function(response) {
+
+   // [4] フロントエンドに対してレスポンスを返す
+   res.send(response.data);
+ })
+ .catch(function(error) {
+   //console.log('ERROR!! occurred in Backend.')
+   res.send({})
+ });
+});
+
+// 通常は3000ポートを使用（ポートを解放する必要あり）
+app.listen('3000', () => {
+ console.log('プログラムスタート');
+});
